f9959508572009da88656a1847c223ede3aa4302
Mally13 <mallymutuku13@gmail.com>
2023-08-24 23:02:49 +0300

    Task 0

--- handle_push.c
+++ handle_push.c
@@ -2,6 +2,29 @@
 #include <stdbool.h>
 
 /**
+ * is_integer - checks if a string represents a valid integer
+ * @str: the string to check
+ * Return: true if the string is a valid integer, false otherwise
+ */
+bool is_integer(const char *str)
+{
+	int i = 0;
+
+	if (str == NULL || *str == '\0')
+		return (false);
+	if (str[0] == '-')
+		i++;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (!isdigit(str[i]))
+			return (false);
+	}
+
+	return (true);
+}
+
+/**
  * handle_push - adds n element to the top of the stack
  * @head: pointer to the head of a stack
  * @line_number: line number of the monty instructions file
@@ -12,7 +35,7 @@ void handle_push(stack_t **head, unsigned int line_number)
 	stack_t *new_node;
 	int n_value;
 
-	if (arg == NULL)
+	if (arg == NULL || is_integer(arg))
 	{
 		dprintf(2, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
