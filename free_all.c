0e93cfbe3c2cb4fd1e1e79b949d20421df53498e
Mally13 <mallymutuku13@gmail.com>
2023-08-25 01:48:49 +0300

    fix: Betty

--- free_all.c
+++ free_all.c
@@ -2,22 +2,21 @@
 
 /**
  * free_all - Frees the memory associated with the stack nodes
- * @head: Double pointer to the head of the stack
  */
 void free_all(void)
 {
-	stack_t *current = *stack;
-	stack_t *next;
+    stack_t *current = *stack;
+    stack_t *next;
 
-	if (stack == NULL || *stack == NULL)
-		return;
+    if (stack == NULL || *stack == NULL)
+        return;
 
-	while (current != NULL)
-	{
-		next = current->next;
-		free(current);
-		current = next;
-	}
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
 
-	*stack = NULL;
+    *stack = NULL;
 }
